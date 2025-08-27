989cc411695ed535e17512eb677e4ee1a7fbcf74
kairy11 <kairy.le0629@gmail.com>
2025-08-27 13:56:27 -0500

    just starting only created a funtion for generating random number

---
+++ script.js
@@ -0,0 +1,12 @@
+let humanScore = 0;
+let computerScore = 0;
+let currentRoundNumber = 1;
+
+// Write your code below:
+const generateTarget = () => {
+    return Math.floor(Math.random() * 10);
+}
+
+const compareGuesses = () => {
+    
+}
